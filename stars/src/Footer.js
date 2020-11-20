09d82c4ec6ddd4440429996f61a780d56953e44e
dameonA <dameon.adolphin@gmail.com>
2020-11-20 02:43:35 -0800

    Adding some adding chnges to main

--- stars/src/Footer.js
+++ stars/src/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Footer() {
+function Footer() {
   return (
     <footer className="Footer">
       <p>
@@ -9,3 +9,5 @@ export default function Footer() {
     </footer>
   );
 }
+
+export default Footer;
